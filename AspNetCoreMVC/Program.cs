aa5733296a157bb729c7684ba94bc662ae44788c
zhenfq <zhenfq@litsoft.com.cn>
2019-02-14 09:38:42 +0800

    Add ASP.NET Core MVC project

---
+++ AspNetCoreMVC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AspNetCoreMVC
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run(); //Starts the Kestrel web server
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>(); //References the Startup class in Startup.cs
+    }
+}
